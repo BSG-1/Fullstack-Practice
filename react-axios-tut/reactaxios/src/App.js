a66a8276dda363c7cec2c29ebbde4dfe10c02e52
Ghataorhe <b.ghataorhe@gmail.com>
2018-11-24 19:16:32 -0800

    introduces create.js, index.js to components folder

---
+++ react-axios-tut/reactaxios/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/boostrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        React Axios Tutorial
+      </div>
+    );
+  }
+}
+
+export default App;
