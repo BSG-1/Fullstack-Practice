5dd668c3cfff4e4fb3da95f9cbe1d80fe35559f6
Ghataorhe <b.ghataorhe@gmail.com>
2018-11-24 19:23:28 -0800

    defines routes

--- react-axios-tut/reactaxios/src/App.js
+++ react-axios-tut/reactaxios/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Create from './components/Create';
+import Index from './components/Index';
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/boostrap.min.css';
@@ -13,6 +16,11 @@ class App extends Component {
             <li><Link to={'/create'}>Create</Link></li>
             <li><Link to={'/index'}>Index</Link></li>
           </ul>
+          <hr />
+          <Switch>
+            <Route exact path='/create' component={Create} />
+            <Route exact path='/index' component={Index} />
+          </Switch>
         </div>
       </Router>
     );
